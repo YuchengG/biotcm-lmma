13fa3e9857e83c16f3951dc105b95211018c86ad
Aidi Stan <aidistan@live.com>
2015-03-10 21:26:14 +0800

    Implement a basic LMMA

--- lib/biotcm-lmma.rb
+++ lib/biotcm-lmma.rb
@@ -1,5 +1,7 @@
+require 'erb'
 require 'biotcm'
 require 'optparse'
+require 'tempfile'
 
 # A combined literature mining and microarray analysis approach to construct
 # gene networks of a specific biological system.
@@ -8,6 +10,10 @@ class BioTCM::Apps::LMMA < BioTCM::Apps::App
 
   # Current version
   VERSION = "0.1.0"
+
+  attr_reader :lm_node, :lm_edge
+  attr_reader :ma_edge
+  attr_reader :lmma_edge
 
   # Terminal entry
   def run
@@ -21,13 +27,48 @@ class BioTCM::Apps::LMMA < BioTCM::Apps::App
   # @param context [BioTCM::Databases::Medline]
   #   a medline object representing the context for LM-based network construction
   # @param opts [Hash]
-  # @option opts [String] :method only 'co-occurrence' by now
   # @option opts [String] :node_savepath
   # @option opts [String] :edge_savepath
-  # @return [Hash]
+  # @return [self]
   def lm(context, opts = {})
     raise ArgumentError unless context.is_a?(BioTCM::Databases::Medline)
+
     lm_cooccurrence(context, opts)
+
+    return self
+  end
+  # Build MA-based network
+  # @param datapath [String] path to microarray data
+  # @param opts [Hash]
+  # @return [self]
+  def ma(datapath, opts = {})
+    raise 'File not exists' unless File.exist?(datapath)
+
+    ma_pairwise(datapath, opts)
+
+    return self
+  end
+  # Build LMMA-based network
+  def lmma(context, datapath, lm_opts = {}, ma_opts = {})
+    lm(context, lm_opts)
+
+    # Save LM-based network as the background network
+    f_bgnet = Tempfile.new('bgnet')
+    f_bgnet.write(@lm_edge.to_s)
+    f_bgnet.rewind
+    ma_opts[:bgnet_path] = f_bgnet.path
+
+    ma(datapath, ma_opts)
+
+    # Build LMMA-based network
+    @lmma_edge = Table.new(
+      primary_key: "Source\tTarget",
+      col_keys: @ma_edge.col_keys + @lm_edge.col_keys
+    )
+    @ma_edge.row_keys.each do |rkey|
+      @lmma_edge.row(rkey, @ma_edge.row(rkey).merge(@lm_edge.row(rkey)))
+    end
+
     return self
   end
 
@@ -41,8 +82,8 @@ class BioTCM::Apps::LMMA < BioTCM::Apps::App
     # Download abstracts
     context.download_abstracts(path_to_medlines)
 
-    @lmnet_node = {}
-    @lmnet_edge = {}
+    node = {}
+    edge = {}
     counter  = 0
     pmid     = nil
     abstract = ''
@@ -64,16 +105,16 @@ class BioTCM::Apps::LMMA < BioTCM::Apps::App
         # Identify genes
         gene_sets = sentences.collect { |sentence| detector.detect(sentence) }
 
-        # Update lmnet nodes
+        # Update nodes
         gene_sets.flatten.uniq.each do |gene|
-          if @lmnet_node[gene]
-            @lmnet_node[gene] << pmid
+          if node[gene]
+            node[gene] << pmid
           else
-            @lmnet_node[gene] = [pmid]
+            node[gene] = [pmid]
           end
         end
 
-        # Update lmnet edges
+        # Update edges
         gene_sets.collect do |gene_set|
           gene_set.product(gene_set)
         end.inject([]) {|a, b| a + b}.collect do |gene_pair|
@@ -81,31 +122,52 @@ class BioTCM::Apps::LMMA < BioTCM::Apps::App
         end.uniq.each do |gene_pair|
           next if gene_pair[0] == gene_pair[1]
           key = gene_pair.join("\t")
-          if @lmnet_edge[key]
-            @lmnet_edge[key] << pmid
+          if edge[key]
+            edge[key] << pmid
           else
-            @lmnet_edge[key] = [pmid]
+            edge[key] = [pmid]
           end
         end
       end
     end
 
-    # Export
-    if(opts[:node_savepath])
-      tab = Table.new(primary_key: 'Symbol',
-                      col_keys:   ['Occurrence count', 'PMID(s)'])
-      @lmnet_node.keys.sort_by { |k| - @lmnet_node[k].size }.each do |k|
-        tab.row(k, [@lmnet_node[k].size, @lmnet_node[k].join(', ')])
-      end
-      tab.save(opts[:node_savepath])
+    # Build tables
+    @lm_node = Table.new(
+      primary_key: 'Symbol',
+      col_keys:   ['Occurrence count', 'PMID(s)']
+    )
+    node.keys.sort_by { |k| - node[k].size }.each do |k|
+      @lm_node.row(k, [node[k].size, node[k].join(', ')])
     end
-    if(opts[:edge_savepath])
-      tab = Table.new(primary_key: "Source\tTarget",
-                      col_keys:   ['Co-occurrence count', 'PMID(s)'])
-      @lmnet_edge.keys.sort_by { |k| - @lmnet_edge[k].size }.each do |k|
-        tab.row(k, [@lmnet_edge[k].size, @lmnet_edge[k].join(', ')])
-      end
-      tab.save(opts[:edge_savepath])
+    @lm_edge = Table.new(
+      primary_key: "Source\tTarget",
+      col_keys:   ['Co-occurrence count', 'PMID(s)']
+    )
+    edge.keys.sort_by { |k| - edge[k].size }.each do |k|
+      @lm_edge.row(k, [edge[k].size, edge[k].join(', ')])
+    end
+
+    # Export
+    @lm_node.save(opts[:node_savepath]) if(opts[:node_savepath])
+    @lm_edge.save(opts[:edge_savepath]) if(opts[:edge_savepath])
+  end
+  # Use pairwise method to build MA-based network
+  def ma_pairwise(datapath, opts = {})
+    f_script = Tempfile.new('script')
+    f_result = Tempfile.new('result')
+
+    tp = File.read(File.expand_path('../biotcm-lmma/pairwise.R.erb', __FILE__))
+    f_script.write(ERB.new(tp).result(binding))
+    f_script.rewind
+    `Rscript #{f_script.path}`
+
+    @ma_edge = Table.new(primary_key: "Source\tTarget")
+    colname = f_result.gets.chomp.split("\t")[2]
+    f_result.each do |line|
+      col = line.chomp.split("\t")
+      next if @ma_edge.row("#{col[0]}\t#{col[1]}")
+      next if @ma_edge.row("#{col[1]}\t#{col[0]}")
+      @ma_edge.ele(col[0..1].sort.join("\t"), colname, col[2])
     end
   end
 end
--- test/test_biotcm_lmma.rb
+++ test/test_biotcm_lmma.rb
@@ -4,18 +4,25 @@ $LOAD_PATH.unshift(File.expand_path('../../lib', __FILE__))
 require 'biotcm-lmma'
 
 describe BioTCM::Apps::LMMA do
-  describe 'when used to construct LM-based network' do
-    before do
-      @lmma = BioTCM::Apps::LMMA.new
-      @context = BioTCM::Databases::Medline.new('psoriasis') & 'breast cancer'
-    end
-
-    it 'could use co-occurrence information' do
-      @lmma.lm(@context, method:'co-occurrence',
-               node_savepath:'node.tab', edge_savepath:'edge.tab')
-    end
+  before do
+    @lmma     = BioTCM::Apps::LMMA.new
+    @context  = BioTCM::Databases::Medline.new('psoriasis') & 'breast cancer'
+    @datapath = File.expand_path('../test_biotcm_lmma/fake.tab', __FILE__)
   end
 
-  describe 'when used to construct MA-based network' do
+  it 'could build LM-based network' do
+    assert(@lmma.lm(@context))
+    assert_instance_of(Table, @lmma.lm_node)
+    assert_instance_of(Table, @lmma.lm_edge)
+  end
+
+  it 'could build MA-based network' do
+    assert(@lmma.ma(@datapath))
+    assert_instance_of(Table, @lmma.ma_edge)
+  end
+
+  it 'could build LMMA-based network' do
+    assert(@lmma.lmma(@context, @datapath))
+    assert_instance_of(Table, @lmma.lmma_edge)
   end
 end
